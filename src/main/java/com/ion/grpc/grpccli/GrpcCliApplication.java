cdbdc15e69a4c3d1bf574d712233fb36eac334b9
Ion Cucos <cucosion@gmail.com>
2019-04-10 00:26:28 +0300

    first commit

---
+++ src/main/java/com/ion/grpc/grpccli/GrpcCliApplication.java
@@ -0,0 +1,13 @@
+package com.ion.grpc.grpccli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcCliApplication.class, args);
+    }
+
+}
